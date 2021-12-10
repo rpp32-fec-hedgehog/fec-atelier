7e5ad5f70df2397b5fb122235009ff5783c2b206
DominicMonares <dominicsmonares@gmail.com>
2021-12-09 17:34:23 -0800

    create server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('../client/dist'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(port, () => {
+  console.log('Listening on port 3000');
+});
