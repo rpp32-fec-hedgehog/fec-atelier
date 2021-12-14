6b262599be8696c1c496a9a9ae638023bf6b2591
DominicMonares <dominicsmonares@gmail.com>
2021-12-13 20:45:12 -0800

    complete testing infrastructure setup

---
+++ tests/Ratings/test.Ratings.js
@@ -0,0 +1,19 @@
+var assert = chai.assert,
+expect = chai.expect,
+should = chai.should();
+
+// import file(s) needed
+
+describe('Ratings', function() {
+
+  describe('Example', function() {
+
+    it('should show a test here', function() {
+      var doesThisWork = true;
+
+      expect(doesThisWork).to.equal(true);
+    })
+
+  })
+
+})
