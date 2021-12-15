077ce0e78e2a08db244724d7be42153eb9e18d90
Dwight Doran <dwight.d.doran@gmail.com>
2021-12-14 21:59:54 -0600

    added test script in package.json // added file testServer.js // changed test.Overview.js to show tests in terminal with script

--- tests/Overview/test.Overview.js
+++ tests/Overview/test.Overview.js
@@ -1,8 +1,16 @@
-var assert = chai.assert,
-expect = chai.expect,
-should = chai.should();
+const chai = require('chai')
+const assert = chai.assert;
+const axios = require('axios');
+let server = require('../testServer.js')
 
+
+expect = chai.expect;
+should = chai.should();
 // import file(s) needed
+
+// import getProductDataByItem from ('../../utils/apiCalls.js');
+const getProductDataByItem = require('../../utils/apiCalls.js').getProductDataByItem;
+
 
 describe('Overview', function() {
 
@@ -12,6 +20,34 @@ describe('Overview', function() {
       var doesThisWork = true;
 
       expect(doesThisWork).to.equal(true);
+    })
+
+  })
+
+  describe('Gather API data', function() {
+
+    it('should GET (ratings numbers) from Reviews API for StarRating component', function() {
+      // logic to check if data is being recieved from reviews API
+    })
+
+    it('should GET (name, category and price data) from Products API for StarRating component', function() {
+      // logic to check if data is being recieved from products API
+    })
+
+    it('should GET (thumbnails) from Products API for StyleSelector component', function() {
+      // logic to check if data is being recieved from products API
+    })
+
+    it('should GET (image url) from Products API for ImageGallery component', function() {
+      // logic to check if data is being recieved from products API
+    })
+
+    it('should GET (size count and quantity) from Products API for AddToCart component', function() {
+      // logic to check if data is being recieved from products API
+    })
+
+    it('should POST Cart data', function() {
+      // logic to check if data is being posted to cart API
     })
 
   })
