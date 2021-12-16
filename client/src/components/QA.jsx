520e06e8550f9484ad92c12c636375e0b0e4f469
DominicMonares <dominicsmonares@gmail.com>
2021-12-15 16:58:08 -0800

    minor test setup changes

---
+++ client/src/components/QA.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import $ from 'jquery';
+
+class QA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: '/qa/questions/product_id=1',
+      method: 'GET',
+      success: (data) => {
+        console.log('Server GET Success ', DATA);
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Put your components here
+      </div>
+    )
+  }
+}
+
+export default QA;
--- tests/QA/test.QA.js
+++ tests/QA/test.QA.js
@@ -1,8 +1,13 @@
-var assert = chai.assert,
-expect = chai.expect,
+const chai = require('chai');
+const assert = chai.assert;
+const axios = require('axios');
+let server = require('../testServer.js');
+
+expect = chai.expect;
 should = chai.should();
 
 // import file(s) needed
+
 
 describe('Questions & Answers', function() {
 
