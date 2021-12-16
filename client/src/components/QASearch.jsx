fdc13e363cfe891cf5942d1c086560f5c62fe671
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 00:06:30 -0800

    add semicolon

--- client/src/components/QASearch.jsx
+++ client/src/components/QASearch.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 let QASearch = (props) => {
   return <div className="qa-search-bar">
     <input type="text" onChange={props.searchHandler}></input>
-  </div>
+  </div>;
 }
 
 export default QASearch;
