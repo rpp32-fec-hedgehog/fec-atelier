b0d39c9469e59a2e79b91463e7be5fff30c453ea
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 21:15:03 -0800

    refactor example

--- env/config.example.js
+++ env/config.example.js
@@ -1,7 +1,9 @@
 // Put your parse application keys here!
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('Authorization', 'GITHUB_API_TOKEN');
-});
 
-// Put your campus prefix here
-window.CAMPUS = 'hr-rpp32';
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader('Authorization', 'GITHUB_API_TOKEN');
+// });
+
+module.exports = {
+  API_KEY: 'PUT YOUR API KEY HERE'
+}
