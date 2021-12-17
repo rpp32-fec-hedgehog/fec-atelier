51486af750b6e5d8d5518acd6e187c2708ee6910
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 22:32:08 -0800

    refactor map to match correct question structure

--- client/src/components/QA/components/QAView.jsx
+++ client/src/components/QA/components/QAView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 let QAView = (props) => {
   return <ul>
     {props.questions.slice(0, 3).map((q) => {
-      return <li>{q}</li>;
+      return <li key={q.question_id}>{q.question_body}</li>;
     })}
   </ul>;
 }
