67a3cedb2bf715864e3b1bf0921a0313794be4e7
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 16:49:11 -0800

    create server file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 console.log(path.join(__dirname, '..', '/client/dist'));
 
@@ -15,6 +15,8 @@ app.use(express.urlencoded({extended: true}));
 //   res.end();
 // })
 
-app.listen(port, () => {
-  console.log('Listening on port 3000');
-});
+// app.listen(port, () => {
+//   console.log('Listening on port 3000');
+// });
+
+module.exports = app;
