06821b0d8032677e77762da94301b6704ef929eb
DominicMonares <dominicsmonares@gmail.com>
2021-12-17 15:34:21 -0800

    remove old test files

--- tests/QA/QA.test.js
+++
@@ -1,7 +0,0 @@
-const QA = require("../../client/src/components/QA");
-// import QA from "../client/src/QA";
-
-test('jest test', function() {
-  console.log(QA);
-  expect(QA.testAdd()).toBe(4);
-})
