5821567abbc314c1ecfa931dc901a54486ca2075
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 22:34:49 -0800

    minor changes for consistency

--- env/config.example.js
+++ env/config.example.js
@@ -1,9 +1,7 @@
-// Put your parse application keys here!
-
 // $.ajaxPrefilter(function (settings, _, jqXHR) {
 //   jqXHR.setRequestHeader('Authorization', 'GITHUB_API_TOKEN');
 // });
 
 module.exports = {
-  API_KEY: 'PUT YOUR API KEY HERE'
+  API_KEY: 'GITHUB_API_TOKEN'
 }
