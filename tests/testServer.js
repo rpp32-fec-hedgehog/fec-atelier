3442d1b4e3bb12f708f828171272e863471fb8c0
DominicMonares <dominicsmonares@gmail.com>
2021-12-16 17:43:06 -0800

    refactor testServer and testApp

--- tests/testServer.js
+++ tests/testServer.js
@@ -1,6 +1,6 @@
-const app = require('./testApp.js');
+const app = require('./testApp.js')(app);
 const port = 8080;
 
 app.listen((port), () => {
-  console.log(`listening on port ${port});
+  console.log(`listening on port ${port}`);
 })
