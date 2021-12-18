aa890771708cd0d0af5238fc4ddaaad60c9f3bfb
Jarrod <jarrod83miller@gmail.com>
2021-12-18 01:24:46 -0600

    Related Items WIP

---
+++ client/src/components/RelatedProducts.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Hello Everyone</h1>
+    )
+  }
+}
+
+export default RelatedProducts;
