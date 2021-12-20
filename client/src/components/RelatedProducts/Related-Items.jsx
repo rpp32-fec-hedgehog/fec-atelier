eaffc80617d71b23e8995aa9a2e7b59ad7c197b1
Jarrod <jarrod83miller@gmail.com>
2021-12-20 01:23:39 -0600

    related data retrieved

--- client/src/components/RelatedProducts/Related-Items.jsx
+++ client/src/components/RelatedProducts/Related-Items.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import $ from 'jquery';
+import ProductCard from './Product-Card.jsx';
 
 class RelatedItems extends React.Component {
   constructor(props) {
     super(props)
-
+    this.state = {
+      currentItem : 90,
+      relatedItems : []
+    };
   }
 
+  componentDidMount() {
+    $.ajax({
+      url: '/relatedItems',
+      method: 'GET',
+      data: {item_id: this.state.currentItem},
+      success: data => {
+        console.log('Related Item data: ', data);
+      }
+    })
+  }
+
+  render() {
+    return (<ProductCard relatedItems={this.state.relatedItems}/>)
+  }
 }
+
+export default RelatedItems;
