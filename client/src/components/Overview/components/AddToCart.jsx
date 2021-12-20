930a7788b624546138d78de8870dab2820b68781
Dwight Doran <dwight.d.doran@gmail.com>
2021-12-20 14:31:40 -0600

    size selector on AddToCart implemented

--- client/src/components/Overview/components/AddToCart.jsx
+++ client/src/components/Overview/components/AddToCart.jsx
@@ -5,12 +5,13 @@ class AddToCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sku : 2122777,
-      size : '',
+      sku : 2122777, // purely for testing
+      size : '-',
       quantity : 1,
       cart: {}
     };
     this.addToCart = this.addToCart.bind(this);
+    this.selectSize = this.selectSize.bind(this);
   }
 
   addToCart() {
@@ -23,11 +24,31 @@ class AddToCart extends React.Component {
     })
   }
 
+  selectSize(e) {
+    this.setState({
+      size : e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="add-to-cart-info">Add To Cart Area
-        <div>Size Drop down</div>
-        <div>Quantity Drop down</div>
+      <div className="size-selector"> Select a Size
+        <select value={this.state.value} onChange={this.selectSize}>
+          <option value="default">-</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+      </div>
+      <div className="qty-selector"> Select a Quantity
+        <select>
+          <option value="default">-</option>
+        </select>
+      </div>
         <button onClick={this.addToCart}>Add To Cart</button>
       </div>
     )
