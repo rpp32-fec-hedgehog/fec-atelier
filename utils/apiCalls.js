e5c1ec7fd62456db1a9fac639a46707e755244f0
Dwight Doran <dwight.d.doran@gmail.com>
2021-12-20 09:20:54 -0600

    replaced QA_API_KEY with process.env.API_KEY so QA recieves data and everything renders

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -79,7 +79,7 @@ const getProductQuestionData = (product_id, callback) => {
 
   return axios.get(endpoint, {
     headers: {
-      // "Authorization": QA_API_KEY
+      "Authorization" : process.env.API_KEY
     }
   })
     .then((res) => {
