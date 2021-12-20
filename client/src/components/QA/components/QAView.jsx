5c6bff847ea3b0f4494b4655b3541c81506e5bb8
DominicMonares <dominicsmonares@gmail.com>
2021-12-19 18:40:40 -0800

    remove parens from map parameter

--- client/src/components/QA/components/QAView.jsx
+++ client/src/components/QA/components/QAView.jsx
@@ -3,7 +3,7 @@ import Answers from './Answers.jsx';
 
 let QAView = (props) => {
   return <ul>
-    {props.questions.slice(0, 2).map((q) => {
+    {props.questions.slice(0, 2).map(q => {
       return <li key={q.question_id}>
         {q.question_body}
         <Answers answers={q.answers} />
