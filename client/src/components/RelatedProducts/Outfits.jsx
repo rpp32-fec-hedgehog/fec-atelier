1f5e10c5ca9a1f3115a704a60b94d3c1499ee448
Jarrod <jarrod83miller@gmail.com>
2021-12-19 23:19:52 -0600

    added components related-products

---
+++ client/src/components/RelatedProducts/Outfits.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Outfits extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+
+  }
+}
+
+export default Outfits;
---
+++ client/src/components/RelatedProducts/Related-Items.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class RelatedItems extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+}
