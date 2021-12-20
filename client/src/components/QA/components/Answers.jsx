dc398830e64880d8362dbbade80a5f66bfaddc76
DominicMonares <dominicsmonares@gmail.com>
2021-12-19 18:59:30 -0800

    add username and date

--- client/src/components/QA/components/Answers.jsx
+++ client/src/components/QA/components/Answers.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import _ from 'underscore';
+import moment from 'moment';
 
 let Answers = (props) => {
   return <ul>
-    {_.values(props.answers).slice(0, 2).map((a) => {
-      return <li key={a.id}>{a.body}</li>
+    {_.values(props.answers).slice(0, 2).map(a => {
+      return <div key={'a-'.concat(a.id)}>
+        <li >A: {a.body}</li>
+          by {a.answerer_name}, {moment(a.date).format('LL')}
+      </div>
     })}
   </ul>
 }
