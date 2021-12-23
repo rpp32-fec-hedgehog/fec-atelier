8182573ba7b35b7e5aa583c3130ba3bc6bc14835
Jarrod <jarrod83miller@gmail.com>
2021-12-22 23:01:39 -0600

    Fixing merge issues with ratings component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,8 +5,6 @@ import $ from 'jquery';
 import Overview from './components/Overview/Overview.jsx';
 import QA from './components/QA/QA.jsx';
 import Ratings from './components/Ratings.jsx';
-import RelatedProducts from './components/RelatedProducts.jsx';
-import Ratings from './components/Ratings/Ratings.jsx';
 import RelatedItems from './components/RelatedProducts/Related-Items.jsx';
 import Outfits from './components/RelatedProducts/Outfits.jsx';
 
