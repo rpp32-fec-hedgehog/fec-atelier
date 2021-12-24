613507aaa5b5f39a9c03b33200bba18341f650bf
DominicMonares <dominicsmonares@gmail.com>
2021-12-23 20:28:37 -0800

    remove less button

--- client/src/components/QA/components/Questions.jsx
+++ client/src/components/QA/components/Questions.jsx
@@ -21,14 +21,11 @@ let Questions = (props) => {
 
   let totalQs = props.totalQs;
   let more = <button key="more-q" onClick={props.handleQs}>More Answered Questions</button>;
-  let less = <button key="less-q" onClick={props.handleQs}>Less Answered Questions</button>;
 
-  if (totalQs < 2) {
-    return base;
-  } else if (totalQs > questions.length) {
+  if (totalQs > questions.length && totalQs > 2) {
     return base.concat(more);
   } else {
-    return base.concat(less);
+    return base;
   }
 }
 
