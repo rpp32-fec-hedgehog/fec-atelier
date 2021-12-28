31ee7c2c414f0ace704afc788c589bf51b17e855
Rob Stafford <uwillgo2themoon@gmail.com>
2021-12-28 08:41:49 -0800

    Majority of data visible, basic formatting

--- client/src/components/Ratings/components/IndividualReview.jsx
+++ client/src/components/Ratings/components/IndividualReview.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 const IndividualReview = (props) => {
-  // console.log('individual items in individual review: ', props);
-    //const id = props.rating.review_id;
+
+    const date = props.date;
     const summary = props.summary;
+    const body = props.body;
+    const reviewer_name = props.reviewer_name;
+    const recommend = props.recommend;
+    const response = props.response;
 
     return (
       <li>
-      <span>{summary}</span>
+      <span> star rating {moment(date).format('LL')} <br></br> <b>{summary}</b> <br></br> {body} <br></br> {reviewer_name} <br></br> {response} <br></br> {recommend}</span>
     </li>
     )
 
--- client/src/components/Ratings/components/RatingsList.jsx
+++ client/src/components/Ratings/components/RatingsList.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import IndividualReview from './IndividualReview.jsx';
 
 const RatingsList = (props) => {
-  // console.log('props.ratings: ', props.ratings);
 
   let ratings = props.ratings.map((rating) => {
     return (
-      <IndividualReview key={rating.review_id} summary={rating.summary}></IndividualReview>
+      <IndividualReview key={rating.review_id} summary={rating.summary} date={rating.date} body={rating.body} recommend={rating.recommend} reviewer_name={rating.reviewer_name} response={rating.response} helpfulness={rating.helpfulness}></IndividualReview>
     )
   });
 
