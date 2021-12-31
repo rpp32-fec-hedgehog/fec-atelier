25211ed695cf0f8d3779370076aa864a1c71238c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2021-12-30 20:01:39 -0800

    refactor to work with AnswerList, remove class

--- client/src/components/QA/components/Questions.jsx
+++ client/src/components/QA/components/Questions.jsx
@@ -1,53 +1,30 @@
 import React from 'react';
 import _ from 'underscore';
-import Answers from './Answers.jsx';
+import AnswerList from './AnswerList.jsx';
 
-class Questions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: 2
-    }
-  }
+let Questions = (props) => {
+  let questions = props.questions;
+  let base = [<div data-testid="questions" key="q-base">
+    <ul>
+      {questions.map(q => {
+        return <div data-testid={q.question_body} key={q.question_body}>
+          <li key={'q-'.concat(q.question_id)}>
+            Q: {q.question_body}
+            <AnswerList answers={q.answers} />
+          </li>
+        </div>
+      })}
 
-  sortAnswers(question) {
-    return _.chain(_.values(question.answers))
-      .sortBy(answer => { return answer.helpfulness })
-      .reverse()
-      .partition(user => {
-        if (user.answerer_name === 'Seller') {
-          return true;
-        }
-      })
-      .flatten()
-      .slice(0, this.state.expanded)
-      .value()
-  }
+    </ul>
+  </div>];
 
-  render() {
-    let questions = this.props.questions;
-    let base = [<div data-testid="questions" key="q-base">
-      <ul>
-        {questions.map(q => {
-          return <div data-testid={q.question_body} key={q.question_body}>
-            <li key={'q-'.concat(q.question_id)}>
-              Q: {q.question_body}
-              <Answers answers={this.sortAnswers(q)} />
-            </li>
-          </div>
-        })}
+  let totalQs = props.totalQs;
+  let more = <button key="more-q" onClick={props.handleQs}>More Answered Questions</button>;
 
-      </ul>
-    </div>];
-
-    let totalQs = this.props.totalQs;
-    let more = <button key="more-q" onClick={this.props.handleQs}>More Answered Questions</button>;
-
-    if (totalQs > questions.length && totalQs > 2) {
-      return base.concat(more);
-    } else {
-      return base;
-    }
+  if (totalQs > questions.length && totalQs > 2) {
+    return base.concat(more);
+  } else {
+    return base;
   }
 }
 
