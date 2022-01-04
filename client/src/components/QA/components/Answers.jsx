f163977dfa9f3e0c8496c4f1fa53fe919b265c39
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-03 22:36:59 -0800

    refactor to class

--- client/src/components/QA/components/Answers.jsx
+++ client/src/components/QA/components/Answers.jsx
@@ -3,14 +3,15 @@ import _ from 'underscore';
 import $ from 'jquery';
 import moment from 'moment';
 
-let Answers = (props) => {
-  let answer = props.answer;
-  let answerer = answer.answerer_name;
-  if (answerer === 'Seller') {
-    answerer = <b>{answerer}</b>
+class Answers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reported: false
+    };
   }
 
-  let answerIsHelpful = (e) => {
+  answerIsHelpful(e) {
     e.preventDefault();
 
     let answerClasses = e.target.className;
@@ -23,7 +24,7 @@ let Answers = (props) => {
       url: `/qa/answers/${answer_id}/helpful`,
       method: 'PUT',
       success: () => {
-        props.updateAHelp(answer_id, props.question_id);
+        this.props.updateAHelp(answer_id, this.props.question_id);
       },
       error: err => {
         throw err;
@@ -31,17 +32,68 @@ let Answers = (props) => {
     })
   }
 
-  return <ul className="answer" data-testid="answers">
-    <div key={'a-'.concat(answer.id)} data-testid={answerer}>
-      <li className="answer-body">A: {answer.body}</li>
-      <span className="answerer">by {answerer}, {moment(answer.date).format('LL')}</span>
-      <span className="a-helpful">Helpful?</span>
-      <span className={`a-help-count a-help-${answer.id}-${answer.helpfulness}`}
-        onClick={answerIsHelpful}>
-        Yes{`(${answer.helpfulness})`}
-      </span>
-    </div>
-  </ul>
+  render() {
+    let answer = this.props.answer;
+    let answerer = answer.answerer_name;
+    if (answerer === 'Seller') {
+      answerer = <b>{answerer}</b>
+    }
+
+    return (<ul className="answer" data-testid="answers">
+      <div key={'a-'.concat(answer.id)} data-testid={answerer}>
+        <li className="answer-body">A: {answer.body}</li>
+        <span className="answerer">by {answerer}, {moment(answer.date).format('LL')}</span>
+        <span className="a-helpful">Helpful?</span>
+        <span className={`a-help-count a-help-${answer.id}-${answer.helpfulness}`}
+          onClick={this.answerIsHelpful.bind(this)}>
+          Yes{`(${answer.helpfulness})`}
+        </span>
+        <span className="report">Report</span>
+      </div>
+    </ul>)
+  }
 }
+
+// let Answers = (props) => {
+//   let answer = props.answer;
+//   let answerer = answer.answerer_name;
+//   if (answerer === 'Seller') {
+//     answerer = <b>{answerer}</b>
+//   }
+
+//   let answerIsHelpful = (e) => {
+//     e.preventDefault();
+
+//     let answerClasses = e.target.className;
+//     let secondClass = answerClasses.split(' ')[1];
+//     let subClasses = secondClass.split('-');
+//     let answer_id = Number(subClasses[2]);
+//     let questionHelpCount = subClasses[3];
+
+//     $.ajax({
+//       url: `/qa/answers/${answer_id}/helpful`,
+//       method: 'PUT',
+//       success: () => {
+//         props.updateAHelp(answer_id, props.question_id);
+//       },
+//       error: err => {
+//         throw err;
+//       }
+//     })
+//   }
+
+//   return <ul className="answer" data-testid="answers">
+//     <div key={'a-'.concat(answer.id)} data-testid={answerer}>
+//       <li className="answer-body">A: {answer.body}</li>
+//       <span className="answerer">by {answerer}, {moment(answer.date).format('LL')}</span>
+//       <span className="a-helpful">Helpful?</span>
+//       <span className={`a-help-count a-help-${answer.id}-${answer.helpfulness}`}
+//         onClick={answerIsHelpful}>
+//         Yes{`(${answer.helpfulness})`}
+//       </span>
+//       <span className="report">Report</span>
+//     </div>
+//   </ul>
+// }
 
 export default Answers;
