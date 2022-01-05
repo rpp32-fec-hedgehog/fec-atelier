28f3cc7adfcde1734f714f8ff7724a1ad79e1f1f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-04 20:31:55 -0800

    add to state, import dependencies, implement openModal and closeModal

--- client/src/components/QA/components/AskQuestion.jsx
+++ client/src/components/QA/components/AskQuestion.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Modal from 'react-modal';
+import _ from 'underscore';
+import $ from 'jquery';
+
+Modal.setAppElement('#app');
 
 class AskQuestion extends React.Component {
   constructor(props) {
@@ -6,8 +11,32 @@ class AskQuestion extends React.Component {
     this.state = {
       question: '',
       nickname: '',
-      email: ''
+      email: '',
+      product_name: '',
+      modalOpen: false
     };
+  }
+
+  openModal(e) {
+    e.preventDefault();
+    $.ajax({
+      url: `/products/${this.props.product_id}`,
+      method: 'GET',
+      success: data => {
+        this.setState({
+          modalOpen: true,
+          product_name: data.name
+        });
+      },
+      error: err => {
+        alert(err);
+      }
+    })
+  }
+
+  closeModal(e) {
+    e.preventDefault();
+    this.setState({modalOpen: false});
   }
 
   render() {
