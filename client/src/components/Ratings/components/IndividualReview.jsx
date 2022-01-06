4dd81310dce0df062594c60cc7586edb8229cebf
RobStafford <75902183+RobStafford@users.noreply.github.com>
2022-01-05 18:05:28 -0800

    Update IndividualReview.jsx

--- client/src/components/Ratings/components/IndividualReview.jsx
+++ client/src/components/Ratings/components/IndividualReview.jsx
@@ -21,7 +21,10 @@ const IndividualReview = (props) => {
     return (
       <div data-testid="individual-review" className="individual_review">
         <li style={reviewStyle}>
-        <span><StarRating star_rating={star_rating}></StarRating> {reviewer_name}, {moment(date).format('LL')} <br></br> <b>{summary}</b> <br></br> {body} <br></br>...should include pictures<br></br> {response} <br></br> {recommend ? <img src={'images/transparent-background-check-mark.png'} alt='loading' width="10" height="10"/>: null}{recommend ? " I recommend this product.": null}</span>
+        <span><StarRating star_rating={star_rating}></StarRating> {reviewer_name}, {moment(date).format('LL')} <br></br> <b>{summary}</b> <br></br> {body}
+        <br></br>...should include pictures
+        <br></br> {response}
+        <br></br> {recommend ? <img src={'images/transparent-background-check-mark.png'} alt='loading' width="10" height="10"/>: null}{recommend ? " I recommend this product.": null}</span>
         </li>
     </div>
     )
