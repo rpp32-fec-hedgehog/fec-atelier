ab354b1c32337832521e86828afce1283cda3ab6
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-05 16:49:31 -0800

    correct search placeholder

--- client/src/components/QA/components/SearchQuestion.jsx
+++ client/src/components/QA/components/SearchQuestion.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 let SearchQuestion = (props) => {
   return <div data-testid="search-question" className="qa-search-bar">
-    <input type="text" placeholder="Search for answers..." onChange={props.searchQuestions}></input>
+    <input type="text" placeholder="Have a question? Search for answers..." onChange={props.searchQuestions}></input>
   </div>;
 }
 
