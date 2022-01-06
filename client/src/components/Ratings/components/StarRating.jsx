3c6ff9d95761d57ce206bbd5788a379297a201fe
RobStafford <75902183+RobStafford@users.noreply.github.com>
2022-01-06 09:29:58 -0800

    Update StarRating.jsx

--- client/src/components/Ratings/components/StarRating.jsx
+++ client/src/components/Ratings/components/StarRating.jsx
@@ -5,8 +5,6 @@ const StarRating = (props) => {
   const roundedRating = (Math.round(props.star_rating * 4) / 4).toFixed(2);
   const ratingWidth = roundedRating * 15;
 
-  console.log('star rating: ', roundedRating)
-
   return (
     <span>
       <div className="rating"style={{width: ratingWidth}}></div>
