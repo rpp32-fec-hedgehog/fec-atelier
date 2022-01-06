0e48303a1ac4f19c44f2388f1e4d5bbdfb404252
Jarrod <jarrod83miller@gmail.com>
2022-01-05 21:09:41 -0600

    added borders to product cards

--- client/src/components/RelatedProducts/Product-Card.jsx
+++ client/src/components/RelatedProducts/Product-Card.jsx
@@ -4,8 +4,8 @@ import $ from 'jquery';
 const ProductCard = (props) => {
   if (props.type === 'related') {
     return (
-      <div className='related'>
-        <img className="relatedImage" src={props.item.styles[0].photos[0].thumbnail_url} />
+      <div className='product_card related_product'>
+        <img className="related_product_image" src={props.item.styles[0].photos[0].thumbnail_url} />
         <li key={props.i}>{props.item.category}</li>
         <li key={props.i}>{props.item.name}</li>
         <li key={props.i}>{props.item.default_price}</li>
@@ -13,17 +13,20 @@ const ProductCard = (props) => {
       </div>
     )
   } else {
-    <div>
+    return (
+    <div className="product_card">
       <div>
-        <img src=""></img>
+        <div>Pending Item Image</div>
+        <img placeholder="Images Will Import as outfit items are added"/>
       </div>
       <ul>
-        <li key={props.i}>{props.item.category}</li>
-        <li key={props.i}>{props.item.name}</li>
-        <li key={props.i}>{props.item.default_price}</li>
-        <li key={props.i}>{props.item.slogan}</li>
+        <li key={props.i}>Item Catergory</li>
+        <li key={props.i}>Item Name</li>
+        <li key={props.i}>Item Price</li>
+        <li key={props.i}>Fancy Item Slogan</li>
       </ul>
     </div>
+    )
   }
 };
 
