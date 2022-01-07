8d61f9ee735ebadc9dccfa1bfaab13383149105f
Dwight Doran <dwight.d.doran@gmail.com>
2022-01-06 22:12:34 -0600

    checkmarks added to specific images styles when clicked

--- client/src/components/Overview/components/StyleSelector.jsx
+++ client/src/components/Overview/components/StyleSelector.jsx
@@ -1,33 +1,56 @@
 import React from 'react';
 import _ from 'underscore';
+import { Checkmark } from 'react-checkmark';
 
 class StyleSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stylesInfo: ''
+      isChecked: true
     };
+    this.selectStyle = this.selectStyle.bind(this);
+    this.addCheckmark = this.addCheckmark.bind(this);
+  }
+
+  selectStyle(e) {
+    this.props.selectStyle(e);
+    this.addCheckmark(e);
+  }
+
+  addCheckmark(e) {
+    this.setState({
+      isChecked: !this.state.isChecked
+    })
+
+    // console.log(e.target.parentElement)
+    // return e.target.parentElement.insertAdjacentHTML('afterbegin', <Checkmark size='15px' color='blue'/>)
+    // return <Checkmark size='15px' color='blue'/>
+    return true;
   }
 
   render() {
+    const checkmark = <Checkmark size='15px' color='blue'/>
     return (
       <div className="styles-info" data-testid="style-selector">
         <h4>Style {this.props.styleName}</h4>
         <div className="style-img-container">
           {_.map(this.props.styleImgs, (style, index) => {
+            console.log('re render')
             if (index % 4 === 0) {
               return (
-              <span key={index}>
+              <span key={index} className="style-img-background">
                 <br></br>
-                <img className="style-img" id={index} src={style} onClick={this.props.selectStyle}></img>
+                {this.props.selectedStyle === index && checkmark}
+                <img className="style-img" id={index} src={style} onClick={this.selectStyle}></img>
               </span>)
             } else {
               return (
-              <span key={index}>
-                <img className="style-img" id={index} src={style} onClick={this.props.selectStyle}></img>
+              <span key={index} className="style-img-background">
+                {this.props.selectedStyle === index && checkmark}
+                <img className="style-img" id={index} src={style} onClick={this.selectStyle}></img>
               </span>)
             }
-            })}
+          })}
         </div>
       </div>
     )
