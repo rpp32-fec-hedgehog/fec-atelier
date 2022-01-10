4ea33effe5e0003c3b7e883c30c948762cfaff83
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-10 02:19:35 -0800

    complete q-modal input styling

--- client/src/components/QA/components/AskQuestion.jsx
+++ client/src/components/QA/components/AskQuestion.jsx
@@ -113,14 +113,18 @@ class AskQuestion extends React.Component {
   }
 
   render() {
-    const testStyles = {
+    const modalStyle = {
       content: {
         top: '50%',
         left: '50%',
         right: 'auto',
         bottom: 'auto',
+        // width: '50%',
         marginRight: '-50%',
-        transform: 'translate(-50%, -50%)'
+        transform: 'translate(-50%, -50%)',
+        border: '1px solid #000',
+        borderRadius: '0px',
+        fontFamily: 'sans-serif'
       }
     };
 
@@ -132,29 +136,38 @@ class AskQuestion extends React.Component {
         </button>
         <Modal
           isOpen={this.state.modalOpen}
-          style={testStyles}
+          style={modalStyle}
           contentLabel="Ask Your Question"
         >
-          <h2>Ask Your Question</h2>
-          <h4>About the {this.state.product_name}</h4>
+          <div className="q-modal-top">
+            <span className="q-modal-ask">Ask your question about the {this.state.product_name}</span>
+            <span className="close-qa-modal" onClick={this.closeModal.bind(this)}>X</span>
+          </div>
           <div className="invalid-q">{this.state.invalid}</div>
-          <form>
-          <label htmlFor="your-question">{'Your Question (mandatory)'}</label>
-            <textarea className="qa-modal your-question" maxLength="1000"
-              onChange={this.handleQuestion.bind(this)}>
-            </textarea>
-            <label htmlFor="nickname-q">{'What is your nickname? (mandatory)'}</label>
-            <input type="text" className="qa-modal nickname-q" maxLength="60" placeholder="Example: jackson11!"
-              onChange={this.handleNickname.bind(this)}>
-            </input>
-            <div className="privacy-msg">For privacy reasons, do not use your full name or email address</div>
-            <label htmlFor="email-q">{'Your Email (mandatory)'}</label>
-            <input type="text" className="qa-modal email-q" maxLength="60" placeholder="Example: jack@email.com"
-              onChange={this.handleEmail.bind(this)}>
-            </input>
-            <div className="privacy-msg">For authentication reasons, you will not be emailed</div>
-          </form>
-          <button onClick={this.closeModal.bind(this)}>Close</button>
+          <div className="q-modal-form">
+            <div className="q-modal-input">
+              <label className="your-q-label" htmlFor="your-q">{'Your Question (mandatory)'}</label>
+              <textarea className="your-q" maxLength="1000"
+                onChange={this.handleQuestion.bind(this)}>
+              </textarea>
+            </div>
+            <div className="q-modal-sub-form">
+              <div className="q-modal-input">
+                <label className="nickname-q-label" htmlFor="nickname-q">{'What is your nickname? (mandatory)'}</label>
+                <input type="text" className="nickname-q" maxLength="60" placeholder="Example: jackson11!"
+                  onChange={this.handleNickname.bind(this)}>
+                </input>
+                <div className="privacy-msg">For privacy reasons, do not use your full name or email address</div>
+              </div>
+              <div className="q-modal-input">
+                <label className="email-q-label" htmlFor="email-q">{'Your Email (mandatory)'}</label>
+                <input type="text" className="qa-modal email-q" maxLength="60" placeholder="Example: jack@email.com"
+                  onChange={this.handleEmail.bind(this)}>
+                </input>
+                <div className="privacy-msg">For authentication reasons, you will not be emailed</div>
+              </div>
+            </div>
+          </div>
           <button onClick={this.submitQuestion.bind(this)}>Submit Question</button>
         </Modal>
       </div>
