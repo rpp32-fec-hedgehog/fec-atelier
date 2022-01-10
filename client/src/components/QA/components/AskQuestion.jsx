af79bbb6f9da68df0c7b0bf65517c0b8546f519c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-10 04:09:08 -0800

    comment out setAppElement

--- client/src/components/QA/components/AskQuestion.jsx
+++ client/src/components/QA/components/AskQuestion.jsx
@@ -5,7 +5,7 @@ import $ from 'jquery';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
 
-Modal.setAppElement('#app');
+// Modal.setAppElement('#app');
 
 class AskQuestion extends React.Component {
   constructor(props) {
