f815ad94c26506859c4a7fc66c7e434d07784afa
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-10 20:13:32 -0800

    update sample data

--- tests/samples/sample-qa-data.js
+++ tests/samples/sample-qa-data.js
@@ -160,7 +160,207 @@ const product1Data = [
   }
 ];
 
+const product1DataSorted = [
+  {
+    "question_id": 553725,
+    "question_body": "Do you ship internationally?",
+    "question_date": "2021-11-18T00:00:00.000Z",
+    "asker_name": "Jack",
+    "question_helpfulness": 5,
+    "reported": false,
+    "answers": {
+      "5181391": {
+        "id": 5181391,
+        "body": "Yes, we do!",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "test",
+        "helpfulness": 2,
+        "photos": [
+          "https://ucarecdn.com/064c6c85-f077-4b3a-8975-15b47a2cde6d/",
+          "https://ucarecdn.com/46fb5051-9c8e-4198-b96a-c2366dcedb9c/"
+        ]
+      },
+      "5181528": {
+        "id": 5181528,
+        "body": "aaa",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "aa",
+        "helpfulness": 0,
+        "photos": []
+      },
+      "5181540": {
+        "id": 5181540,
+        "body": "yessssss we doooooo",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "lolla",
+        "helpfulness": 1,
+        "photos": [
+          "https://ucarecdn.com/388f03ec-a8f3-4bed-8cae-d733139bf807/-/preview/300x300/-/quality/lighter/-/format/webp/"
+        ]
+      },
+      "5181542": {
+        "id": 5181542,
+        "body": "why?",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "Seller",
+        "helpfulness": 0,
+        "photos": []
+      }
+    }
+  },
+  {
+    "question_id": 553705,
+    "question_body": "new sldkfjlsekjflsijefsef",
+    "question_date": "2021-11-17T00:00:00.000Z",
+    "asker_name": "helsdfdsflo",
+    "question_helpfulness": 2,
+    "reported": false,
+    "answers": {
+      "5181410": {
+        "id": 5181410,
+        "body": "GOOD",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "bizza",
+        "helpfulness": 2,
+        "photos": [
+          "https://ucarecdn.com/78916c78-7056-4c54-b8f1-dc4634980428/"
+        ]
+      },
+      "5181422": {
+        "id": 5181422,
+        "body": "I think this is really good product",
+        "date": "2021-11-19T00:00:00.000Z",
+        "answerer_name": "tesh",
+        "helpfulness": 3,
+        "photos": []
+      },
+      "5181545": {
+        "id": 5181545,
+        "body": "maybe",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "weewoo",
+        "helpfulness": 4,
+        "photos": [
+          "http://res.cloudinary.com/dpwwavsdm/image/upload/v1638600117/IMG_3154_j0bauv.jpg",
+          "http://res.cloudinary.com/dpwwavsdm/image/upload/v1638600118/neeko_laaosh.jpg"
+        ]
+      },
+      "5181546": {
+        "id": 5181546,
+        "body": "aaaaaa",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "aa",
+        "helpfulness": 0,
+        "photos": [
+          "http://res.cloudinary.com/dpwwavsdm/image/upload/v1638600562/heart_kawnsb.png"
+        ]
+      },
+      "5181547": {
+        "id": 5181547,
+        "body": "final test",
+        "date": "2021-12-04T00:00:00.000Z",
+        "answerer_name": "weewoo",
+        "helpfulness": 0,
+        "photos": [
+          "http://res.cloudinary.com/dpwwavsdm/image/upload/v1638600892/pngegg_gqvbid.png"
+        ]
+      }
+    }
+  }
+];
+
 const product2Data = [
+  {
+    "question_id": 553704,
+    "question_body": "really, really, really",
+    "question_date": "2021-11-17T00:00:00.000Z",
+    "asker_name": "hello",
+    "question_helpfulness": 4,
+    "reported": false,
+    "answers": {
+      "5181392": {
+        "id": 5181392,
+        "body": "What do you want to ask?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "jack",
+        "helpfulness": 2,
+        "photos": []
+      },
+      "5181393": {
+        "id": 5181393,
+        "body": "I think you might want to ask, Is this good?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "abby",
+        "helpfulness": 1,
+        "photos": []
+      },
+      "5181394": {
+        "id": 5181394,
+        "body": "Right?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "anny",
+        "helpfulness": 1,
+        "photos": []
+      },
+      "5181456": {
+        "id": 5181456,
+        "body": "good good good good pizza shoes",
+        "date": "2021-11-21T00:00:00.000Z",
+        "answerer_name": "pizza",
+        "helpfulness": 0,
+        "photos": [
+          "https://ucarecdn.com/87015f51-7e80-4761-833c-74581e0be910/"
+        ]
+      }
+    }
+  },
+  {
+    "question_id": 553705,
+    "question_body": "SORT THIS Q",
+    "question_date": "2021-11-17T00:00:00.000Z",
+    "asker_name": "hello",
+    "question_helpfulness": 1,
+    "reported": false,
+    "answers": {
+      "5181392": {
+        "id": 5181392,
+        "body": "What do you want to ask?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "jack",
+        "helpfulness": 2,
+        "photos": []
+      },
+      "5181393": {
+        "id": 5181393,
+        "body": "I think you might want to ask, Is this good?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "abby",
+        "helpfulness": 1,
+        "photos": []
+      },
+      "5181394": {
+        "id": 5181394,
+        "body": "Right?",
+        "date": "2021-11-18T00:00:00.000Z",
+        "answerer_name": "anny",
+        "helpfulness": 1,
+        "photos": []
+      },
+      "5181456": {
+        "id": 5181456,
+        "body": "good good good good pizza shoes",
+        "date": "2021-11-21T00:00:00.000Z",
+        "answerer_name": "pizza",
+        "helpfulness": 0,
+        "photos": [
+          "https://ucarecdn.com/87015f51-7e80-4761-833c-74581e0be910/"
+        ]
+      }
+    }
+  }
+];
+
+const product2DataSorted = [
   {
     "question_id": 553704,
     "question_body": "really, really, really",
@@ -253,5 +453,7 @@ const answerData = {
 };
 
 module.exports.product1Data = product1Data;
+module.exports.product1DataSorted = product1DataSorted;
 module.exports.product2Data = product2Data;
+module.exports.product2DataSorted = product2DataSorted;
 module.exports.answerData = answerData;
