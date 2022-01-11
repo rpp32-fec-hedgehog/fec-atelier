577088dc0231dc4b05220fb2c38ac8832c33c88e
Dwight Doran <dwight.d.doran@gmail.com>
2022-01-11 15:44:08 -0600

    images appear in n rows with 4 max columns and a checkmark in upperleft corner of image

--- client/src/components/Overview/components/StyleSelector.jsx
+++ client/src/components/Overview/components/StyleSelector.jsx
@@ -14,23 +14,14 @@ class StyleSelector extends React.Component {
     const checkmark = <Checkmark size='15px' color='#8e9efa'/>
     return (
       <div className="styles-info" data-testid="style-selector">
-        <h4>Style {this.props.styleName}</h4>
+        <h4 className="style-name">Style {this.props.styleName}</h4>
         <div className="style-img-container">
           {_.map(this.props.styleImgs, (style, index) => {
-            if (index % 4 === 0) {
-              return (
-              <span key={index} className="style-img-background">
-                <br></br>
-                {this.props.selectedStyle === index && checkmark}
-                <img className="style-img" id={index} src={style} onClick={this.props.selectStyle}></img>
-              </span>)
-            } else {
               return (
               <span key={index} className="style-img-background">
                 {this.props.selectedStyle === index && checkmark}
                 <img className="style-img" id={index} src={style} onClick={this.props.selectStyle}></img>
               </span>)
-            }
           })}
         </div>
       </div>
