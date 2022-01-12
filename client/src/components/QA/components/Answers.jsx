f32cc6c518deaa4e5472e66dbd9614b4f1c53d5f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-11 23:24:28 -0800

    implement thumbnail modal

--- client/src/components/QA/components/Answers.jsx
+++ client/src/components/QA/components/Answers.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Modal from 'react-modal';
 import _ from 'underscore';
 import $ from 'jquery';
 import moment from 'moment';
+
+Modal.setAppElement('#app');
 
 class Answers extends React.Component {
   constructor(props) {
@@ -9,7 +12,9 @@ class Answers extends React.Component {
     this.state = {
       reported: false,
       votes: [],
-      thumbnails: <></>
+      thumbnails: <></>,
+      modalOpen: false,
+      currentImg: ''
     };
   }
 
@@ -54,16 +59,27 @@ class Answers extends React.Component {
     })
   }
 
-  clickThumbnail(e) {
+  openModal(e) {
     e.preventDefault();
+    let image = e.target.src;
+    this.setState({
+      modalOpen: true,
+      currentImg: image
+    })
+  }
 
+  closeModal(e) {
+    e.preventDefault();
+    this.setState({
+      modalOpen: false
+    })
   }
 
   componentDidMount() {
     let answer = this.props.answer;
     if (answer.photos.length > 0) {
       this.setState({thumbnails: <div className="a-thumbnails">{_.map(answer.photos, photo => {
-        return <img className="a-thumbnail" key={photo} src={photo} onClick={this.clickThumbnail.bind(this)}></img>
+        return <img className="a-thumbnail" key={photo} src={photo} onClick={this.openModal.bind(this)}></img>
       })}</div>})
     }
   }
@@ -77,9 +93,11 @@ class Answers extends React.Component {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
-        border: '1px solid #000',
+        border: '0px',
         borderRadius: '0px',
-        fontFamily: 'sans-serif'
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        fontFamily: 'sans-serif',
+        overflow: 'auto'
       }
     };
 
@@ -114,6 +132,16 @@ class Answers extends React.Component {
           {reportButton}
         </div>
       </li>
+      <Modal
+        isOpen={this.state.modalOpen}
+        style={modalStyle}
+        contentLabel="View Image"
+      >
+        <div className="a-thumbnail-modal">
+          <span className="close-a-thumbnail" onClick={this.closeModal.bind(this)}>X</span>
+          {<img className="a-clicked-thumbnail" src={this.state.currentImg}></img>}
+        </div>
+      </Modal>
     </ul>)
   }
 }
