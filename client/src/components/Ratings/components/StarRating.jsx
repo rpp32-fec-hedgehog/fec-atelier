787b7c3accdc2994fe720bfedeb5d4f4ecc07a11
Rob Stafford <uwillgo2themoon@gmail.com>
2022-01-14 08:22:36 -0800

    Fix for NaN warning

--- client/src/components/Ratings/components/StarRating.jsx
+++ client/src/components/Ratings/components/StarRating.jsx
@@ -5,10 +5,15 @@ const StarRating = (props) => {
   const roundedRating = (Math.round(props.star_rating * 4) / 4).toFixed(2);
   const ratingWidth = roundedRating * 15;
 
-  return (
-      <div className="rating" style={{width: ratingWidth}}></div>
+  if (isNaN(ratingWidth)) {
+    return (
+      <div className="rating"></div>
   );
-
+  } else {
+    return (
+        <div className="rating" style={{width: ratingWidth}}></div>
+    );
+  }
 }
 
 export default StarRating;
