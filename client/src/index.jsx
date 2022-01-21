224972706cc8296d4311b6b2a7b1ac0153b1d6a8
RobStafford <75902183+RobStafford@users.noreply.github.com>
2022-01-20 17:40:04 -0800

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,8 +18,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item_id: Math.floor(Math.random() * (60563 - 59553 + 1)) + 59553,
-      //item_id: 59557,
+      //item_id: Math.floor(Math.random() * (60563 - 59553 + 1)) + 59553,
+      item_id: 59557,
       myOutfit: [],
       darkmode: false
     };
